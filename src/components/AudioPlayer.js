d05483eab10f6af1a21934fa409ddaa4c4a5e188
MS <156176+mattsilv@users.noreply.github.com>
2025-03-13 21:22:04 -0400

    Remove unused Typography import to fix ESLint error

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -2,7 +2,6 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
 
 import TimeSlider from "./audioPlayer/TimeSlider";
 import VolumeControl from "./audioPlayer/VolumeControl";
